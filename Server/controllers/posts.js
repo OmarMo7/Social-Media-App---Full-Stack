5c01f63f13c9500b53553a14856de7b627060995
OmarMo7 <omar7.first@gmail.com>
2022-05-02 17:45:01 +0200

    Establishing get & create post methods

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -1,5 +1,26 @@
-// import axios from 'axios'
-// const url = ""
+import postMesseges from '../models/postSchema'
 
-export const getPosts = () => {}
+
+export const getPosts = (req, res) => {
+  try {
+    const postCollection = await postMesseges.find()
+    res.status(200).json(postCollection)
+  } catch (error) {
+    res.status(404).json(error)
+  }
+}
+
+export const createPosts = (req, res) => {
+  const post = req.body
+  const newPost = new postMesseges(post)
+  
+  try {
+    await newPost.save()
+    res.status(201).json(newPost)
+  } catch (error) {
+    res.status(403).json({
+      messege: error
+    })
+  }
+}
 
