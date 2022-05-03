96bc308a744ebfa79ee3e6bce8229b768f04f031
OmarMo7 <omar7.first@gmail.com>
2022-05-03 02:33:13 +0200

    Using .env to store critical info

--- Server/index.js
+++ Server/index.js
@@ -2,15 +2,17 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 const app = express()
 
+dotenv.config()
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use('posts', postRoutes)
-const CONNECTION_URL = 'mongodb+srv://OmarMostafa:ooooooo7@cluster0.ebavy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 8000
 
 mongoose.connect(CONNECTION_URL)
