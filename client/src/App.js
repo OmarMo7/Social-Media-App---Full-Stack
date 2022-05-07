691e98d3177ab3c8c28d0db883aedae754bfab9a
OmarMo7 <omar7.first@gmail.com>
2022-05-07 02:39:30 +0200

    styling the big app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,44 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { Container, AppBar, Typography, Grid, Grow } from '@material-ui/core'
+import falcon from "./images/falcon.jpg"
+
+import { useDispatch } from 'react-redux';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
+
 
 const App = () => {
-  return (
-    <div>
-      <h1>
-        Hello
-      </h1>
-    </div>
-  )
-}
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
 
-export default App
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">My App</Typography>
+        <img className={classes.image} src={falcon} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
