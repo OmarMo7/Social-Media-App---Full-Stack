8f21d62e8323ca63f25a8e3aeed6f8c7d4b1b0ee
OmarMo7 <omar7.first@gmail.com>
2022-05-28 14:57:45 +0200

    Creating auth reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,9 +1,12 @@
-export default (state = {authData: null}, action) => {
+export default (state = { authData: null }, action) => {
   switch (action.type) {
     case "AUTH":
-      localStorage.setItem('profile', JSON.stringify({...action?.data}))
-      return {...state, };
-    
+      localStorage.setItem('profile', JSON.stringify({ ...action?.data }))
+      return { ...state, authData: action?.data };
+    case "LOGOUT":
+      localStorage.clear()
+      return { ...state, authData: null };
+
     default:
       return posts;
   }
