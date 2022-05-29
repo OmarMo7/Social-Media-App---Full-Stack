8197853ad52691ba0f0e1799ed1d139c12d4155c
OmarMo7 <omar7.first@gmail.com>
2022-05-29 23:49:20 +0200

    Buikding the middleware to ensure user auth

---
+++ Server/middleware/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]
+
+    const isCustomLogging = token.length < 500
+
+    var decodedData = null
+
+    if (token && isCustomLogging) {
+      decodedData = jwt.verify(token, 'test token')
+      req.userId = decodedData?.id
+    }
+    else {
+      decodedData = jwt.decode(token)
+      req.userId = decodedData?.sub
+    }
+
+    next()
+  } catch (error) {
+    console.log(error)
+  }
+} 
