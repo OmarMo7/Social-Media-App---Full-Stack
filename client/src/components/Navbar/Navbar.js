8f16ba57f5da130d6b4bad3bc1021e510e86c76e
OmarMo7 <omar7.first@gmail.com>
2022-06-02 17:17:48 +0200

    Adding logout button - Fixing the bug of navbar auto-population when user exists

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,23 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core'
-import useStyles from './styles';
-import { Link } from 'react-router-dom' 
+import { Link, useHistory, useLocation } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
 import falcon from "../../images/falcon.jpg"
-import { useHistory } from 'react-router-dom';
+import useStyles from './styles';
+import decode from 'jwt-decode';
+
 
 const Navbar = () => {
   const dispatch = useDispatch()
   const classes = useStyles()
   const history = useHistory()
+  const location = useLocation()
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
 
   const logout = () => {
-    dispatch({type: 'LOGOUT'})
+    dispatch({ type: 'LOGOUT' })
     history.push('/')
 
     setUser(null)
   }
+
+
+  useEffect(() => {
+    const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location]);
+
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
@@ -31,7 +47,7 @@ const Navbar = () => {
               <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl} > {user.result.name.charAt(0)}
               </Avatar>
               <Typography className={classes.username} variant='h6'>{user.result.name}</Typography>
-              <Button className={classes.logout} variant='contained' color='secondary'>Logout</Button>
+              <Button className={classes.logout} variant='contained' color='secondary' onClick={logout}>Logout</Button>
             </div>) : (
               <Button component={Link} to='/auth' variant='contained' color='primary'>Sign Up</Button>
             )
