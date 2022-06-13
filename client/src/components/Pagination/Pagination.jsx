a231b3ab7c12677e16726ae8f4aa50f17136c0e8
OmarMo7 <omar7.first@gmail.com>
2022-06-13 23:38:10 +0200

    Creating pagination component with its styles

---
+++ client/src/components/Pagination/Pagination.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Pagination, PaginationItem } from '@material-ui/lab'
+import { Link } from 'react-router-dom'
+
+import useStyles from './styles'
+
+const Paginate = () => {
+  const classes = useStyles()
+  return (
+    <div>
+      <Pagination 
+        classes={{ul: classes.ul}}
+        count={5}
+        page={1}
+        variant="outlined"
+        color="primary"
+        renderItem={(item)=>{
+            <PaginationItem {...item} component={Link} to={`/posts?pages=${1}`}/>
+        }}
+
+      />
+    </div>
+  )
+}
