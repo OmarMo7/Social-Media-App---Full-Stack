a5f8cc6ebc7c8cea5d63ebb6cee485d311667e01
OmarMo7 <omar7.first@gmail.com>
2022-06-15 23:56:12 +0200

    Adjusting sime paths - not letting an auth user to go '/auth'

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import React from "react"
 import { Container } from '@material-ui/core'
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 
 const App = () => {
 
+  const user = JSON.parse(localStorage.getItem("profile"))
 
   return (
     <BrowserRouter>
       <Container maxWidth="lg">
         <Navbar />
         <Switch>
-          <Route component={Home} exact path="/" />
-          <Route component={Auth} exact path="/auth" />
+          <Route component={() => (<Redirect to={"/posts"} />)} exact path="/" />
+          <Route component={Home} exact path="/posts" />
+          <Route path="/posts/search" component={Home} exact />
+          <Route path="/posts/:id" component={PostDetails} exact />
+          <Route component={() => (!user ? <Auth /> : <Redirect to={"/posts"} />)} exact path="/auth" />
         </Switch>
       </Container>
     </BrowserRouter>
