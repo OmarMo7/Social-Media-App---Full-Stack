1093c5db073c7db59a7af8bb53fc09585f54ffdd
OmarMo7 <omar7.first@gmail.com>
2022-06-30 23:46:53 +0200

    Adding a route for searching

--- Server/routes/posts.js
+++ Server/routes/posts.js
@@ -1,12 +1,13 @@
 import express from "express"
 import {
-  getPosts, createPost, getPost, updatePost, deletePost, likePost
+  getPosts, createPost, getPost, updatePost, deletePost, likePost, getPostsBySearch
 } from "../controllers/posts.js"
 
 import auth from '../middleware/auth.js'
 
 const router = express.Router()
 
+router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
 router.post('/', auth, createPost);
 // router.get('/:id', getPost);
