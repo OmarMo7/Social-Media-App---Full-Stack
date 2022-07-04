1f096aaaabfd404afd6709cfb5d80dc7af6a2055
OmarMo7 <omar7.first@gmail.com>
2022-07-04 23:03:10 +0200

    Adding the api for fetchPost & fetchBySearch

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -11,7 +11,9 @@ API.interceptors.request.use((req) => {
 
 })
 
-export const fetchPosts = () => API.get('/posts');
+export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
+export const fetchPost = (id) => API.get(`/posts/${id}`);
+export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search}&tags=${searchQuery.tags}`);
 export const createPost = (newPost) => API.post('/posts', newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
