90485fda3d40ecf451366c66769b751640bcec24
OmarMo7 <omar7.first@gmail.com>
2022-07-09 08:44:59 +0200

    Set isLoading variable to control loading status

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,35 +1,27 @@
 import React from 'react';
 import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
 
 import Post from './Post/Post';
 import useStyles from './styles';
 
-const setCircularProgress = (loading) => {
-  return loading ? (<CircularProgress />) : (<div>
-    You have no posts yet
-  </div>);
-}
 
 
 const Posts = ({ setCurrentId }) => {
-  const [loading, setLoading] = useState(true);
-  const posts = useSelector((state) => state.posts);
+  const { posts, isLoading } = useSelector((state) => state.posts);
   const classes = useStyles();
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false)
-    }, 3000)
-  }, [loading]);
+  
+  if (!posts.length && !isLoading){
+    return 'You have no posts yet'
+  }
+  
 
   return (
-    !posts.length ? setCircularProgress(loading) : (
+    isLoading ? <CircularProgress /> : (
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-        {posts.map((post) => (
-          <Grid key={post._id} item xs={12} sm={6} md={6}>
-            <Post post={post} setCurrentId={setCurrentId} />
+        {posts?.map((post) => (
+          <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
+            <Post post={post} setCurrentId={setCurrentId}  />
           </Grid>
         ))}
       </Grid>
