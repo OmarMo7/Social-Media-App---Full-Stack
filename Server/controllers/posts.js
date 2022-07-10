23388c69ca1c5c140dbbb02e31b97e99db2af32c
OmarMo7 <omar7.first@gmail.com>
2022-07-10 12:02:14 +0200

    Adding the commentPost functionality

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -126,5 +126,23 @@ export const likePost = async (req, res) => {
   res.status(200).json(updatedPost);
 }
 
+export const commentPost = async (req, res) => {
+  const { id } = req.params
+  const { value } = req.body
+
+  try {
+    const post = await PostMessage.findById(id)
+
+    post?.comments.push(value)
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true })
+
+    res.status(201).json(updatedPost)
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
 
 export default router;
