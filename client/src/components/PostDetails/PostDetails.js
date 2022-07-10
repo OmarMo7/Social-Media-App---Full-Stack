fd77854bf790d3248bac90350a0bb92692641026
OmarMo7 <omar7.first@gmail.com>
2022-07-10 12:03:45 +0200

    Adding the recommended posts section

--- client/src/components/PostDetails/PostDetails.js
+++ client/src/components/PostDetails/PostDetails.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
 import { Paper, Typography, CircularProgress, Divider } from '@material-ui/core'
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import { useDispatch, useSelector } from 'react-redux'
 import moment from 'moment'
 import { useParams, useHistory } from 'react-router-dom'
 import useStyles from './styles'
-import { getPost } from '../../actions/posts'
+import { getPost, getPostsBySearch } from '../../actions/posts'
+import CommentSection from './CommentSection';
 const PostDetails = () => {
   const dispatch = useDispatch()
   const history = useHistory()
@@ -12,9 +14,16 @@ const PostDetails = () => {
   const { post, posts, isLoading } = useSelector((state) => state.posts)
   const { id } = useParams()
 
+  const openPost = (_id) => {
+    history.push(`/posts/${_id}`)
+  }
   useEffect(() => {
     dispatch(getPost(id))
   }, [id])
+
+  useEffect(() => {
+    if (post) dispatch((getPostsBySearch({ search: 'none', tags: post?.tags.join(',') })))
+  }, [post])
 
   if (!post) return null
   console.log(isLoading)
@@ -26,6 +35,7 @@ const PostDetails = () => {
     );
   }
 
+  const otherPosts = posts.filter(({ _id }) => post._id !== _id)
 
   return (
     <Paper style={{ padding: '20px', borderRadius: '15px' }} elevation={6}>
@@ -37,13 +47,35 @@ const PostDetails = () => {
           <Typography variant="h6">Created by: {post.name}</Typography>
           <Typography variant="body1">{moment(post.createdAt).fromNow()}</Typography>
           <Divider style={{ margin: '20px 0' }} />
+          <Typography variant="body1">{post.messege}</Typography>
+          <Divider style={{ margin: '20px 0' }} />
           <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
+          <Divider style={{ margin: '20px 0' }} />
+          <CommentSection post={post} />
           <Divider style={{ margin: '20px 0' }} />
         </div>
         <div className={classes.imageSection}>
           <img className={classes.media} src={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} alt={post.title} />
         </div>
       </div>
+
+      {
+        otherPosts && (<div className={classes.section}>
+          <Typography gutterBottom variant='h5'> You might also like: </Typography>
+          <Divider />
+          <div className={classes.recommendedPosts}>
+            {otherPosts.map((post) => (
+              <div style={{ margin: "20px", cursor: "pointer" }} onClick={() => { openPost(post._id) }} key={post._id} >
+                <Typography gutterBottom variant='h6'>{post.title}</Typography>
+                <Typography gutterBottom variant='body1'>{post.name}</Typography>
+                <Typography gutterBottom variant='body1'>{post.message}</Typography>
+                <Typography gutterBottom variant='h6'><ThumbUpAltOutlined fontSize="small" /> {post.likes.length}</Typography>
+                <img src={post.selectedFile} width={"100px"} />
+              </div>
+            ))}
+          </div>
+        </div>)
+      }
     </Paper>
   )
 }
