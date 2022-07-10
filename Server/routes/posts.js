484d9c07de61294abc88d8380d5fb2fd4758c522
OmarMo7 <omar7.first@gmail.com>
2022-07-10 12:01:41 +0200

    Adding the router of post details & comment

--- Server/routes/posts.js
+++ Server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from "express"
 import {
-  getPosts, createPost, getPost, updatePost, deletePost, likePost, getPostsBySearch
+  getPosts, createPost, getPost, updatePost, deletePost, likePost, commentPost, getPostsBySearch
 } from "../controllers/posts.js"
 
 import auth from '../middleware/auth.js'
@@ -9,10 +9,11 @@ const router = express.Router()
 
 router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
+router.get('/:id', getPost);
 router.post('/', auth, createPost);
-// router.get('/:id', getPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
 router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', auth, commentPost);
 
 export default router
