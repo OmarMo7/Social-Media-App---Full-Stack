71afee81c79d79b407400a0cffa7c56d350a07f3
OmarMo7 <omar7.first@gmail.com>
2022-07-11 22:32:42 +0200

    Moving Build folder to the root

---
+++ build/precache-manifest.d560060415c9c74244a26e655e32326e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2ae30a6204f3f0e629f91f03420ecd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61df50c3aa83d18bce75",
+    "url": "/static/js/2.32ab60b2.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/static/js/2.32ab60b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df779be7863c774eebd5",
+    "url": "/static/js/main.f2c6bf94.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "a868732000a5007a5a6091f1e6ba6e6e",
+    "url": "/static/media/falcon.a8687320.jpg"
+  }
+]);
