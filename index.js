9f9f9a05ca10c6cbb1e71732178cd7ea510a4a7c
OmarMo7 <omar7.first@gmail.com>
2022-07-12 01:47:16 +0200

    change 8

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')));
+app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.all('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 app.get('/', (req, res) => {
   res.send("APP IS RUNNING")
 })
-app.use('/posts', postRoutes)
-app.use('/user', userRoutes)
 
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 8000
