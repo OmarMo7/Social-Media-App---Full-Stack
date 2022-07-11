6265a1e757756890105d2667105d4d2e438c7c49
OmarMo7 <omar7.first@gmail.com>
2022-07-11 20:16:12 +0200

    Changing names

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-import postRoutes from './server/routes/posts.js'
-import userRoutes from './server/routes/user.js'
+import postRoutes from './routes/posts.js'
+import userRoutes from './routes/user.js'
 
 const app = express()
 
