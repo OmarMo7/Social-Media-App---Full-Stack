4717e2378e57947aba3d87df12fd1fe0753786a8
OmarMo7 <omar7.first@gmail.com>
2022-07-11 16:21:55 +0200

    Separating out the running files

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-import postRoutes from './routes/posts.js'
-import userRoutes from './routes/user.js'
+import postRoutes from './server/routes/posts.js'
+import userRoutes from './server/routes/user.js'
 
 const app = express()
 
-dotenv.config()
+dotenv.config({ path: './server/.env' })
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
