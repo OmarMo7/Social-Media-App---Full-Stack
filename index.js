a2bcf0b1792885be539efd4ca0712d857ddb233f
OmarMo7 <omar7.first@gmail.com>
2022-07-13 21:41:12 +0200

    change 13

--- index.js
+++ index.js
@@ -20,14 +20,15 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 app.use('/posts', postRoutes)
 app.use('/user', userRoutes)
 
 app.get('/', (req, res) => {
   res.send("APP IS RUNNING")
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 const CONNECTION_URL = process.env.CONNECTION_URL
