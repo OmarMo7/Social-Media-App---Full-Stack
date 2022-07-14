17c0c31c2f621adbab059d7241d474d93c3fd169
OmarMo7 <omar7.first@gmail.com>
2022-07-14 14:47:31 +0200

    change working

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.use('/posts', postRoutes)
