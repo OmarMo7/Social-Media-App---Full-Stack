bfb4e4d73a8795818ef3d2e8d1b0b2e746fdbcc8
OmarMo7 <omar7.first@gmail.com>
2022-07-26 20:11:35 +0200

    Adding some callbacks

--- index.js
+++ index.js
@@ -20,11 +20,15 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.use('/posts', postRoutes)
-app.use('/user', userRoutes)
+app.use('/posts', postRoutes, () => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+app.use('/user', userRoutes, postRoutes, () => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
 
 app.get('/', (req, res) => {
   res.send("APP IS RUNNING")
