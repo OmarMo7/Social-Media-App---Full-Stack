df0e0c98b868432907e8ae5c338ac3d9553bb277
OmarMo7 <omar7.first@gmail.com>
2022-07-27 01:27:08 +0200

    Serving the build folder from within client folder

---
+++ client/build/precache-manifest.6cf8871988889ecd7fd4e9313a951872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648c5dc50cf94a514f763d4160cd0e53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0134ad120d570393eed5",
+    "url": "/static/js/2.927d5ad8.chunk.js"
+  },
+  {
+    "revision": "c09ee4cb5c07c907c49515170b08a988",
+    "url": "/static/js/2.927d5ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ebabfab5a722786ab7",
+    "url": "/static/js/main.a2715500.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "a868732000a5007a5a6091f1e6ba6e6e",
+    "url": "/static/media/falcon.a8687320.jpg"
+  }
+]);
