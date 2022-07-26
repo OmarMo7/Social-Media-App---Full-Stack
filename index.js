6e7b8ad08f4d0b8e044111c161ae7d91379d364e
OmarMo7 <omar7.first@gmail.com>
2022-07-26 20:49:24 +0200

    changing routes #7

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')));
-app.use('/posts', postRoutes)
-app.use('/user', userRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/user', userRoutes)
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
