c9fe5ec2f06713518c6178745b1a65c1a6a75342
OmarMo7 <omar7.first@gmail.com>
2022-07-28 17:33:42 +0200

    Changing component attr to render & applying themes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React from "react"
-import { Container } from '@material-ui/core'
+import { Button, Container } from '@material-ui/core'
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 import PostDetails from "./components/PostDetails/PostDetails";
-
+import useStyles from './styles'
 import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
 import { useTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
-
 
 export const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
 
@@ -21,24 +18,27 @@ const App = () => {
 
   const theme = useTheme();
   const colorMode = React.useContext(ColorModeContext);
+  const classes = useStyles()
+
+  document.body.style.backgroundColor = theme.palette.background.default
+
 
   return (
     <BrowserRouter>
-      <Container maxWidth="lg" style={{ backgroundColor: theme.palette.background.default, color: theme.palette.text.primary }}>
+      <Container className={classes.height} style={{ backgroundColor: theme.palette.background.default, color: theme.palette.text.primary }} >
         <Navbar />
-        <div
-        >
+        <Button className={classes.darkModeButton} style={{ backgroundColor: theme.palette.buttons.main }} variant="contained" onClick={colorMode.toggleColorMode}>
           {theme.palette.mode} mode
-          <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode}>
+          <IconButton sx={{ ml: 1 }} >
             {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
           </IconButton>
-        </div>
+        </Button>
         <Switch>
-          <Route component={() => (<Redirect to={"/posts"} />)} exact path="/" />
-          <Route component={() => (<Home theme={theme} />)} exact path="/posts" />
-          <Route path="/posts/search" component={() => (<Home theme={theme} />)} exact />
-          <Route path="/posts/:id" component={() => (<PostDetails theme={theme} />)} exact />
-          <Route component={() => (<Auth />)} exact path="/auth" />
+          <Route render={() => (<Redirect to={"/posts"} />)} exact path="/" />
+          <Route render={() => (<Home theme={theme} />)} exact path="/posts" />
+          <Route path="/posts/search" render={() => (<Home />)} exact />
+          <Route path="/posts/:id" render={() => (<PostDetails />)} exact />
+          <Route render={() => (<Auth />)} exact path="/auth" />
         </Switch>
       </Container>
     </BrowserRouter>
