ced73a7781b54b51f3dff9f2ea528de7aee3e968
OmarMo7 <omar7.first@gmail.com>
2023-03-05 23:24:52 +0200

    Testing removing an api route

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'client', 'build')));
-app.use('/api/posts', postRoutes)
+// app.use('/api/posts', postRoutes)
 app.use('/api/user', userRoutes)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
