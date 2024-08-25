37da51d6e617a4b81bb4b4208ca9a9c823e3aab3
OmarMo7 <omar7.first@gmail.com>
2024-08-25 13:30:00 +0200

    Resolve & Update many dependecies of the client side

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,63 +1,96 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core'
-import { Link, useHistory, useLocation } from 'react-router-dom'
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect, useCallback } from "react";
+import { AppBar, Avatar, Button, Toolbar, Typography } from "@mui/material";
+import { Link, useHistory, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import useStyles from './styles';
-import decode from 'jwt-decode';
-import { useTheme } from '@mui/material/styles'
-
+import useStyles from "./styles";
+import decode from "jwt-decode";
+import { useTheme } from "@mui/styles";
 
 const Navbar = () => {
-  const dispatch = useDispatch()
-  const classes = useStyles()
-  const history = useHistory()
-  const location = useLocation()
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
-  const theme = useTheme()
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const theme = useTheme();
   const token = user?.token;
 
   const logout = useCallback(() => {
-    dispatch({ type: 'LOGOUT' })
-    history.push('/')
+    dispatch({ type: "LOGOUT" });
+    history.push("/");
 
-    setUser(null)
-  }, [dispatch, history])
-
+    setUser(null);
+  }, [dispatch, history]);
 
   useEffect(() => {
-
     if (token) {
       const decodedToken = decode(token);
 
       if (decodedToken.exp * 1000 < new Date().getTime()) logout();
     }
-    setUser(JSON.parse(localStorage.getItem('profile')));
+    setUser(JSON.parse(localStorage.getItem("profile")));
   }, [location, logout, token]);
 
   return (
-    <AppBar className={classes.appBar} position="static" style={{ backgroundColor: theme.palette.appBar.main, color: theme.palette.text.primary }}>
-      <div className={classes.brandContainer} component={Link} to="/" >
+    <AppBar
+      className={classes.appBar}
+      position="static"
+      style={{
+        backgroundColor: theme.palette.appBar.main,
+        color: theme.palette.text.primary,
+      }}
+    >
+      <div className={classes.brandContainer} component={Link} to="/">
         <a href="/">
-          <img className={classes.image} src={theme.palette.images.appBar} alt="fakebook" height="60" />
+          <img
+            className={classes.image}
+            src={theme.palette.images.appBar}
+            alt="fakebook"
+            height="60"
+          />
         </a>
       </div>
       <Toolbar className={classes.toolbar}>
-        {
-          user ?
-            (<div className={classes.profile}>
-              <Avatar className={classes.purple} alt={user?.result?.name} src={user?.result?.imageUrl} > {user?.result?.name.charAt(0)}
-              </Avatar>
-              <Typography className={classes.username} variant='h6'>{user?.result?.name}</Typography>
-              <Button className={classes.logout} variant='contained' color='secondary' onClick={logout}>Logout</Button>
-            </div>) : (location.pathname !== "/auth" ?
-              <Button component={Link} to='/auth' variant='contained' color='primary'>Sign In</Button>
-              : <Button component={Link} to='/' variant='contained' color='primary'>See Posts</Button>
-            )
-        }
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user?.result?.name}
+              src={user?.result?.imageUrl}
+            >
+              {" "}
+              {user?.result?.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.username} variant="h6">
+              {user?.result?.name}
+            </Typography>
+            <Button
+              className={classes.logout}
+              variant="contained"
+              color="secondary"
+              onClick={logout}
+            >
+              Logout
+            </Button>
+          </div>
+        ) : location.pathname !== "/auth" ? (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        ) : (
+          <Button component={Link} to="/" variant="contained" color="primary">
+            See Posts
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
